6217fa4e4a43f050cbfa13e8ecdea326336f880e
AHMAD ASYRAAF DANIAL BIN NORAZLAN 284368 <92504346+AsyraafDanial-284368@users.noreply.github.com>
2021-11-11 13:07:47 +0800

    Create CODING IFSTATEMENT SLIDED 9.java

---
+++ CODING IFSTATEMENT SLIDED 9.java
@@ -0,0 +1,19 @@
+package selfProgramming;
+
+public class IfStatementSlide9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int amount = 350;
+		int balance = 1000;
+	
+		
+		if (amount <= balance )
+			balance = balance-amount;
+		
+		System.out.println(balance);
+		
+		
+	}
+
+}
