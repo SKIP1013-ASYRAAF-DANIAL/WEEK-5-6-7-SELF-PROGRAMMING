4632939ed06966cde471a3b0587bdf4b7f818254
AHMAD ASYRAAF DANIAL BIN NORAZLAN 284368 <92504346+AsyraafDanial-284368@users.noreply.github.com>
2021-11-15 09:59:42 +0800

    Create CODING BlockStatementsSlide14.java

---
+++ CODING BlockStatementsSlide14.java
@@ -0,0 +1,19 @@
+package selfProgramming;
+
+public class BlockStatementsSlide14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int total = 100;
+		int max = 70;
+		int errorCount = 500;
+		
+		if (total>max)
+		{
+			System.out.println ("Error!!");
+			System.out.println(errorCount++);
+		}
+
+	}
+
+}
