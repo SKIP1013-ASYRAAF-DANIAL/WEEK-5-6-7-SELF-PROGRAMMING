1c5ff47202bb869dcbe1e29c84a02cd301d8f3b7
AHMAD ASYRAAF DANIAL BIN NORAZLAN 284368 <92504346+AsyraafDanial-284368@users.noreply.github.com>
2021-11-15 10:17:09 +0800

    Create CODING MultipleSelectionSlide27.java

---
+++ CODING MultipleSelectionSlide27.java
@@ -0,0 +1,30 @@
+package selfProgramming;
+
+public class MultipleSelectionSlide27 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+int a = -25;
+		
+		if (a>=1)
+		{
+			System.out.println("The number you enter is : "+ a);
+			System.out.println("You enter the positive number");
+		}
+		
+		else if (a<0)
+		{
+			System.out.println("The number you enter is : "+ a);
+			System.out.println("You enter the negative number");
+		}
+		
+		else
+		{
+			System.out.println("The number you enter is : "+ a);
+			System.out.println("You enter the zero number");
+		}
+
+	}
+
+}
